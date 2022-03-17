1d50fe2408e3259812cb1f1fee1635c9acfc496b
Carlos Hoyos <84938183+CarlosGadelhaHoyos@users.noreply.github.com>
2022-03-16 20:35:09 -0400

    estrutura do programa
    criado os métodos , construtor, atributos restantes, e métodos especiais.

--- Exercício Pratico - Banco/src/mainPackage/ContaBanco.java
+++ Exercício Pratico - Banco/src/mainPackage/ContaBanco.java
@@ -2,8 +2,58 @@ package mainPackage;
 
 public class ContaBanco {
 
+	//Atributos
 	public int numConta;
 	protected char tipo;
 	private char dono;
-	 
+	private float saldo;
+	private boolean status;
+	
+	//Métodos especiais
+	public ContaBanco() {
+		
+		this.saldo = 0;
+		this.status = false;
+	}
+	
+	public void setNumConta(int n){
+		
+		this.numConta = n;
+	}
+	
+	public int getNumConta() {
+		
+		return this.numConta;
+	}
+	
+	public void setTipo(char t) {
+		
+		this.tipo = t;
+	}
+	
+	//Métodos
+	public void abrirConta() {
+		
+	}
+	
+	public void fecharConta() {
+		
+		
+	}
+	
+	public void depositar() {
+		
+		
+	}
+	
+	public void sacar() {
+		
+		
+	}
+	
+	public void pagarMensal(){
+		
+		
+	}
+	
 }
