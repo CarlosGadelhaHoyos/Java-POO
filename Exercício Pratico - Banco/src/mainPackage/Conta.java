a6bec2e98c939d41bc27ec2f7a6899dd9229055e
Carlos Hoyos <84938183+CarlosGadelhaHoyos@users.noreply.github.com>
2022-03-28 17:25:48 -0400

    Refeito programa inteiro

--- Exercício Pratico - Banco/src/mainPackage/Conta.java
+++ Exercício Pratico - Banco/src/mainPackage/Conta.java
@@ -2,5 +2,174 @@ package mainPackage;
 
 public class Conta {
 
+	public int numConta;
+	protected String tipo;
+	private String dono;
+	private float saldo;
+	private boolean status;
 	
+	//metodos personalizados:
+	
+	public void abrirConta(String t) {
+		
+		this.setStatus(true);
+		this.setTipo(t.toUpperCase().strip());
+		
+		if(this.getTipo().equals("CC")) {
+			
+			this.setSaldo(50);
+			System.out.println("\nConta aberta com sucesso!");
+		}else if(this.getTipo().equals("CP")) {
+			
+			this.setSaldo(150);
+			System.out.println("\nConta aberta com sucesso!");
+		}else {
+			
+			System.out.println("\nERRO\nTipo de conta inválida!\n");
+		}
+	}
+	
+	public void fecharConta() {
+		
+		if(this.getSaldo() > 0) {
+			
+			System.out.println("\nA conta não pode ser fechada porque ainda tem dinheiro.\n");
+		}else if(this.getSaldo() < 0) {
+			
+			System.out.println("\nA conta não pode ser fechada pois esatá em débito.\n");
+		}else{
+			
+			this.setStatus(false);
+		}
+	}
+	
+	public void depositar(float s) {
+		
+		if(this.getStatus() == true) {
+			
+			this.setSaldo(this.getSaldo() + s);
+			System.out.println("\nDepósito realizado com sucesso na conta de " + this.getDono());
+		}else if(this.getStatus() == false) {
+			
+			System.out.println("\nImpossível depositar");
+		}
+	}
+	
+	public void sacar(float v) {
+		
+		if(this.getStatus() == true) {
+			
+			this.setSaldo(this.getSaldo()+ v);
+			System.out.println("\nSaque realizado na conta de " + this.getDono());
+		}else if(this.getSaldo() < v) {
+			
+			System.out.println("\nSaldo insuficiente!");
+		}else {
+			
+			System.out.println("\nImpossivel sacar!");
+		}
+	}
+	
+	public void pagarMensal() {
+		
+		int v = 0;
+		
+		if (this.getTipo() == "cc") {
+			
+			v = 12;
+		}else if(this.getTipo() == "cp") {
+			
+			v = 20;
+		}
+		
+		if(this.getStatus() == true) {
+			
+			this.setSaldo(this.getSaldo() - v);
+			System.out.println("\nMensalidade paga com sucesso por " + this.getDono());
+		}else {
+			
+			System.out.println("\nImpossível pagar!");
+		}
+	}
+	
+	public void estadoAtual() {
+		
+		System.out.println("__________________________________________");
+		System.out.println("Número da conta: " + this.getNumConta());
+		System.out.println("Tipo da conta: " + this.getTipo());
+		System.out.println("Dono da conta: " + this.getDono());
+		System.out.println("Saldo da conta: " + this.getSaldo());
+		System.out.println("Status da conta: " + this.getStatus());
+		System.out.println("__________________________________________");
+	}
+	
+	//Métodos especiais:
+	
+	public Conta() {
+		
+		this.setSaldo(0);
+		this.setStatus(false);
+	}
+	
+	public void setNumConta(int n) {
+		
+		this.numConta = n;
+	}
+	
+	public int getNumConta() {
+		
+		return this.numConta;
+	}
+	
+	public void setDono(String d) {
+		
+		this.dono = d;
+	}
+	
+	public String getDono() {
+		
+		return this.dono;
+	}
+	
+	public void setSaldo(float s) {
+		
+		this.saldo = s;
+	}
+	
+	public float getSaldo() {
+		
+		return this.saldo;
+	}
+	
+	public void setStatus(boolean s) {
+		
+		this.status = s;
+	}
+	
+	public boolean getStatus() {
+		
+		return this.status;
+	}
+	
+	public void setTipo(String t) {
+		
+		this.tipo = t;
+	}
+	
+	public String getTipo() {
+		
+		return this.tipo;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- Exercício Pratico - Banco/src/mainPackage/Main.java
+++ Exercício Pratico - Banco/src/mainPackage/Main.java
@@ -4,6 +4,20 @@ public class Main {
 
 	public static void main(String args[]) {
 		
+		Conta c1 = new Conta();
+		c1.abrirConta("cc");
+		c1.setNumConta(1111);
+		c1.setDono("Jubileu");
 		
+		Conta c2 = new Conta();
+		c2.abrirConta("cp   ");
+		c2.setNumConta(2222);
+		c2.setDono("Creuza");
+		
+		c1.depositar(100);
+		c2.depositar(500);
+		
+		c1.estadoAtual();
+		c2.estadoAtual();
 	}
 }
