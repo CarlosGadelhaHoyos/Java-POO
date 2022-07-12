9bb665466c296fd4f90ed390341257cf656193b4
CarlosHoyos <carlosgadelhahoyos@gmail.com>
2022-07-12 16:30:09 -0400

    Criação dos metodos getters e setters, e métodos construtor e métodos normais
    Criado código do método construtor, e criado todos os métodos setters e getters.

--- UEC/src/Lutadores.java
+++ UEC/src/Lutadores.java
@@ -14,5 +14,109 @@ public class Lutadores {
      * Lista: apresentar, ganhar luta, perder luta, empatar luta e status.
     */
 
+    public Lutadores(String no, String na, int id, float al, float pe, String ca, int vi, int de, int em){
+
+        this.nome = no;
+        this.nacionalidade = na;
+        this.idade = id;
+        this.altura = al;
+        this.setPeso(pe);
+        this.categoria = ca;
+        this.vitorias = vi;
+        this.derrotas = de;
+        this.empates = em;
+    }
+    public void apresentar(){
+
+    }
+
+    public void ganharLuta(){
+
+    }
+
+    public void perderLuta(){
+
+    }
+
+    public void empatar(){
+
+    }
+
+    public void status(){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+    
     
 }
