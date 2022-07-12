fc82059a84df7105e785ba2217a9cb2524db866a
CarlosHoyos <carlosgadelhahoyos@gmail.com>
2022-07-12 15:35:37 -0400

    Criando atributos dos lutadores

---
+++ UEC/src/Lutadores.java
@@ -0,0 +1,13 @@
+public class Lutadores {
+    
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+    
+}
