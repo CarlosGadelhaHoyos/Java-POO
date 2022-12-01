11ce4e8468ee1e261866c69588e274fd7136cf54
CarlosHoyos <carlosgadelhahoyos@gmail.com>
2022-12-01 12:50:53 -0400

    Creating variables
    Created the variables of the Garrafa class

--- ExercicioPessoal_POO_GarrafinhaDeAgua/GarrafinhaDeAgua/src/Garrafa.java
+++ ExercicioPessoal_POO_GarrafinhaDeAgua/GarrafinhaDeAgua/src/Garrafa.java
@@ -1,4 +1,10 @@
 public class Garrafa {
     
-    
+    private boolean capped;
+    private boolean uncapped;
+    private boolean empty;
+    private float bottleCapacity;
+    private String bottleColor;
+    private String capType;
+
 }
