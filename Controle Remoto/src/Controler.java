da8e00ef2cd170443294535dfce0cbd5c9d01964
CarlosHoyos <carlosgadelhahoyos@gmail.com>
2022-12-01 20:05:50 -0400

    Creating Controler interface
    Creating the future methods  of the Remote Controler

---
+++ Controle Remoto/src/Controler.java
@@ -0,0 +1,13 @@
+public interface Controler {
+    
+    public abstract void turnOn();
+    public abstract void turnOff();
+    public abstract void openMenu();
+    public abstract void closeMenu();
+    public abstract void plusVolum();
+    public abstract void minusVolume();
+    public abstract void mute();
+    public abstract void unmute();
+    public abstract void play();
+    public abstract void pause();
+}
