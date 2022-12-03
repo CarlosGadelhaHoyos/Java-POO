748fbf2160b1feb5678bc152ddffdb6a32d8fda8
CarlosHoyos <carlosgadelhahoyos@gmail.com>
2022-12-02 20:17:21 -0400

    RemoteControl code created
    All the logic, special methods (getters and setterrs), and the implemented methods of the Interface were created.

--- Controle Remoto/src/Main.java
+++ Controle Remoto/src/Main.java
@@ -1,3 +1,12 @@
 public class Main {
     
+    public static void main(String[] args){
+        
+        RemoteControl rc = new RemoteControl();
+
+        rc.openMenu();
+        rc.mute();
+        rc.
+        rc.unmute();
+    }
 }
--- Controle Remoto/src/RemoteControl.java
+++ Controle Remoto/src/RemoteControl.java
@@ -1,4 +1,98 @@
-public class RemoteControl {
+public class RemoteControl implements Controler{
 
-    
+    public int volume;
+    public boolean on;
+    public boolean playing;
+
+    public RemoteControl(){
+
+        this.volume = 50;
+        this.on = false;
+        this.playing = false;
+    }
+
+    private int getVolume() {
+        return volume;
+    }
+
+    private void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private boolean isOn() {
+        return on;
+    }
+
+    private void setOn(boolean on) {
+        this.on = on;
+    }
+
+    private boolean isPlaying(){ 
+        return playing;
+    }
+
+    private void setPlaying(boolean playing) {
+        this.playing = playing;
+    }
+
+    public void turnOn() {
+        this.setOn(true);
+    }
+
+    public void turnOff() {
+        this.setOn(false);
+    }
+
+    public void openMenu() {
+        System.out.println("");
+        System.out.println("Esta ligado? "+ this.isOn());
+        System.out.println("Esta tocando? "+ this.isPlaying());
+        System.out.println("Volume: "+ this.getVolume());
+        for(int i = 0; i <= this.getVolume(); i+=10){
+            System.out.print("|");
+        }
+
+    }
+
+    public void closeMenu() {
+        System.out.println("Fechando menu...");
+    }
+
+    public void plusVolum() {
+        if(this.isOn()){
+            this.setVolume(this.getVolume()+5);
+        }
+    }
+
+    public void minusVolume() {
+        if(this.isOn()){
+            this.setVolume(this.getVolume()-5);
+        } else if(this.getVolume() == 0){
+            System.out.println("O volume atual já é 0");
+        }
+    }
+
+    public void mute() {
+        if(this.isOn() && this.getVolume() > 0){
+            this.setVolume(0);
+        }
+    }
+
+    public void unmute() {
+        if(isOn() && this.getVolume() == 0){
+            this.setVolume(50);
+        }
+    }
+
+    public void play() {
+        if(this.isOn() && !(this.isPlaying())){
+            this.setPlaying(true);
+        }
+    }
+ 
+    public void pause() { 
+        if(this.isOn() && this.isPlaying()){
+            this.setPlaying(false);
+        }
+    }
 }
