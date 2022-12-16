5ba699561afa6067f5af65bcec200a9e9f61267b
CarlosHoyos <carlosgadelhahoyos@gmail.com>
2022-12-16 18:30:14 -0400

    Created the constructor and the special methods
    The constructor is finished, but the special methods I'll finish during the lesson.

--- UltraEmojiCombat/src/Lutador.java
+++ UltraEmojiCombat/src/Lutador.java
@@ -6,4 +6,36 @@ public class Lutador{
         private float peso;
         private String categoria;
         private int vitorias, derrotas, empates;
+
+        public Lutador(String no, String na, int id, float al, float pe, int vit, int de, int em){
+
+            this.nome = no;
+            this.nacionalidade = na;
+            this.idade = id;
+            this.altura = al;
+            this.peso = pe;
+            this.peso = vit;
+            this.derrotas = de;
+            this.empates = em;
+        }
+
+        public void apresentar(){
+
+        }
+
+        public void status(){
+
+        }
+
+        public void ganharLuta(){
+
+        }
+
+        public void perderLuta(){
+
+        }
+
+        public void empatarLuta(){
+
+        }
 }
