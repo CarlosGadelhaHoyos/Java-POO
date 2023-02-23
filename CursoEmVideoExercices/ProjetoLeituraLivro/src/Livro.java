d00d3248b29be727efd524edec9c186e6812101f
CarlosHoyos <carlosgadelhahoyos@gmail.com>
2023-02-23 00:13:57 -0400

    Adding the constructor and the main structure

---
+++ CursoEmVideoExercices/ProjetoLeituraLivro/src/Livro.java
@@ -0,0 +1,14 @@
+public class Livro{
+
+    private String titulo;
+    private String autor;
+    private int totPags;
+    private int pagAtual;
+    private boolean aberto;
+    private Pessoa leitor;
+
+    public void detalhes(){
+
+
+    }
+}
--- CursoEmVideoExercices/ProjetoLeituraLivro/src/Pessoa.java
+++ CursoEmVideoExercices/ProjetoLeituraLivro/src/Pessoa.java
@@ -1,11 +1,17 @@
-public class Pessoa {
+public class Pessoa{
     private String nome; 
     private int idade;
     private char sexo;
     private int colecaoLivros;
 
     public void comprarLivro(){
-
         this.colecaoLivros++;
+    }
+
+    public Pessoa(){
+        System.out.println("Qual seu nome?\nR: ");
+        System.out.println("Qual sua idade?\nR: ");
+        System.out.println("Qual seu sexo? Masculino [m], Feminino [f]\nR: ");
+        System.out.println("Quantos livros você tem?\nR: ");
     }
 }
