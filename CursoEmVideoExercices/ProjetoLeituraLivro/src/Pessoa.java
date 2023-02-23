eb770b6781341314c3849ee8a46d2ce561d555ec
CarlosHoyos <carlosgadelhahoyos@gmail.com>
2023-02-23 00:14:32 -0400

    Added getters and setters

--- CursoEmVideoExercices/ProjetoLeituraLivro/src/Pessoa.java
+++ CursoEmVideoExercices/ProjetoLeituraLivro/src/Pessoa.java
@@ -14,4 +14,61 @@ public class Pessoa{
         System.out.println("Qual seu sexo? Masculino [m], Feminino [f]\nR: ");
         System.out.println("Quantos livros você tem?\nR: ");
     }
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return int return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return char return the sexo
+     */
+    public char getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return int return the colecaoLivros
+     */
+    public int getColecaoLivros() {
+        return colecaoLivros;
+    }
+
+    /**
+     * @param colecaoLivros the colecaoLivros to set
+     */
+    public void setColecaoLivros(int colecaoLivros) {
+        this.colecaoLivros = colecaoLivros;
+    }
+
 }
