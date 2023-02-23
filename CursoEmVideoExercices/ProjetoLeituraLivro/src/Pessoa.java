e5ca78d617aad992fc388e05cf4dd7c0ad05e042
CarlosHoyos <carlosgadelhahoyos@gmail.com>
2023-02-22 23:52:44 -0400

    Adding the attributes and the method of Pessoa

--- CursoEmVideoExercices/ProjetoLeituraLivro/src/Pessoa.java
+++ CursoEmVideoExercices/ProjetoLeituraLivro/src/Pessoa.java
@@ -1,3 +1,11 @@
 public class Pessoa {
-    
+    String nome; 
+    int idade;
+    char sexo;
+    int colecaoLivros;
+
+    public void comprarLivro(){
+        
+        this.colecaoLivros++;
+    }
 }
