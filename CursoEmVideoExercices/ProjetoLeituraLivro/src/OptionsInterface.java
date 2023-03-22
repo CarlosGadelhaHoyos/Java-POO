b5abd698bce7cd7bec83a710febefe40be1e2d16
Carlos Hoyos <carlosgadelhahoyos@gmail.com>
2023-03-22 17:29:04 -0400

    Making the constructor of the class OptionsInterface

--- CursoEmVideoExercices/ProjetoLeituraLivro/src/OptionsInterface.java
+++ CursoEmVideoExercices/ProjetoLeituraLivro/src/OptionsInterface.java
@@ -1,3 +1,10 @@
 public class OptionsInterface {
     
+    public OptionsInterface(){
+        System.out.println("\n==== Projeto livro ====\n");
+        System.out.println("Antes de prosseguir é necessario definir os detalhes do usuário e ao menos um livro");
+        Pessoa p = new Pessoa();
+        Livro l = new Livro(p);
+    }
+
 }
