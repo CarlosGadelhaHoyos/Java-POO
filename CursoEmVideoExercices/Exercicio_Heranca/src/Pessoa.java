ce8533363234ccd5893384d73a372936fe63f819
Carlos Hoyos <carlosgadelhahoyos@gmail.com>
2023-07-03 18:40:03 -0400

    Added the structues for Pessoa class

--- CursoEmVideoExercices/Exercicio_Heranca/src/Pessoa.java
+++ CursoEmVideoExercices/Exercicio_Heranca/src/Pessoa.java
@@ -1,3 +1,53 @@
-public class Pessoa {
-    
+public abstract class Pessoa {
+    private String nome;
+    private int idade;
+    private char sexo;
+
+    public final void fazerAniversario(){
+
+        this.setIdade(this.getIdade() + 1);
+    }
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return int return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return char return the sexo
+     */
+    public char getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
 }
