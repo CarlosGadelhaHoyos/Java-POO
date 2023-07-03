9b41be8e84d563e34035c84a6e03787eda8379c6
Carlos Hoyos <carlosgadelhahoyos@gmail.com>
2023-07-03 19:44:55 -0400

    Added the structures of Bolsista class

--- CursoEmVideoExercices/Exercicio_Heranca/src/Bolsista.java
+++ CursoEmVideoExercices/Exercicio_Heranca/src/Bolsista.java
@@ -1,3 +1,15 @@
-public class Bolsista {
+public class Bolsista extends Aluno{
     
+    private int bolsa;
+    
+    @Override
+    public void pagarMensalidade() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void renovarBolsa(){
+
+
+    }
 }
