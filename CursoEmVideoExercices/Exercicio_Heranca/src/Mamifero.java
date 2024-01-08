9dde90a9d4470a67a2d6526866fbf6bb2aad9fb2
CarlosGadelhaHoyos <carlosgadelhahoyos@gmail.com>
2024-01-07 22:31:51 -0400

    Created and builded the class Peixe

--- CursoEmVideoExercices/Exercicio_Heranca/src/Mamifero.java
+++ CursoEmVideoExercices/Exercicio_Heranca/src/Mamifero.java
@@ -15,5 +15,12 @@ public class Mamifero extends Animal{
     @Override
     public void emitirSom() {
         System.out.println("Som de mamifero");
+    }    
+
+    public String getCorPelo() {
+        return corPelo;
+    }
+    public void setCorPelo(String corPelo) {
+        this.corPelo = corPelo;
     }
 }
