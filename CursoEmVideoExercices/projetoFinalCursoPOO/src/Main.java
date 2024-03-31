1761b9fa5ed0088bdf8a2bdd6064cbddcd435a7c
CarlosGadelhaHoyos <carlosgadelhahoyos@gmail.com>
2024-03-30 22:06:51 -0400

    Created the rest project objects

--- CursoEmVideoExercices/projetoFinalCursoPOO/src/Main.java
+++ CursoEmVideoExercices/projetoFinalCursoPOO/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        
+        Video v[] = new Video[3];
+
+        v[0] = new Video("Aula 1 de POO");
+        v[1] = new Video("Alguma coisa ae");
+
+        v[0].showVideoAttributes();
+        v[1 ].showVideoAttributes();
+
+        
     }
 }
