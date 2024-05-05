e4b129c75148d3adcd20da01916ac273ced36fbe
CarlosGadelhaHoyos <carlosgadelhahoyos@gmail.com>
2024-05-05 13:20:35 -0400

    finished the final project of Java OOP course - Curso em Video

--- CursoEmVideoExercices/projetoFinalCursoPOO/src/Main.java
+++ CursoEmVideoExercices/projetoFinalCursoPOO/src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static void main(String[] args){
         
+        // Parei no minuto 12:40 da ultima aula de POO
+
         Video v[] = new Video[3];
 
         v[0] = new Video("Aula 1 de POO");
-        v[1] = new Video("Alguma coisa ae");
+        v[1] = new Video("Integracao banco de dados com Python");
+        v[2] = new Video("Aula 17 de HTML e CSS");
 
-        v[0].showVideoAttributes();
-        v[1 ].showVideoAttributes();
+        Gafanhoto g[] = new Gafanhoto[2];
+        g[0] = new Gafanhoto("jubileu", 12, "M", "apuca");
+        g[1] = new Gafanhoto("creuza", 39, "F", "crezurete");
 
-        
+        Visualizacao vis = new Visualizacao(g[0], v[0]);
+        System.out.println(vis.toString());
     }
 }
